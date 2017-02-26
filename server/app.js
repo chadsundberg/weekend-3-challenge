97142b41e710928f1058e934b52aa9698079cefc
Chad Sundberg <chadsundberg@gmail.com>
2017-02-26 11:24:36 -0600

    base mode

--- server/app.js
+++ server/app.js
@@ -1,17 +1,12 @@
-console.log('sourced');
-
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var calculator = require('./routes/calculator')
 
 app.use(express.static('server/public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
-
-// app.get('//calculator', newMathObject);
 
 app.post('/calculator/new', function(req, res){
   var newCalc = req.body;
@@ -26,11 +21,8 @@ app.post('/calculator/new', function(req, res){
   } else if (newCalc.operation == '/') {
     var solution = x/y;
   }
-  console.log(solution);
-  // res.sendStatus(200);
+  res.send(solution.toString());
 });
-
-
 
 
 app.listen(5000);
--- server/public/client.js
+++ server/public/client.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
 
-var mathOperation = '';
+  var mathOperation = '';
 
-$('.operation').on('click', function() {
-  mathOperation = $(this).val();
-});
+  $('.operation').on('click', function() {
+    mathOperation = $(this).val();
+  });
 
+  $('.clear').on('click', function(){
+    $('#answerDisplay').empty();
+  });
 
   $('form').on('submit', function(event){
     event.preventDefault();
@@ -13,23 +16,21 @@ $('.operation').on('click', function() {
     newMathObject.firstValue = $('#firstValue').val();
     newMathObject.secondValue = $('#secondValue').val();
     newMathObject.operation = mathOperation;
-
     saveNewCalculation(newMathObject);
-    // console.log(newMathObject);
-});
-    function saveNewCalculation(newestMath) {
-      $.ajax({
-        type: 'POST',
-        url: '/calculator/new',
-        data: newestMath, // jquery expects this data property to be an object
-        success: function(response){
-          console.log(response);
-        },
-        error: function(response) {
-          // $('#firstFishy').text('error, you missed a name!');
-        }
-      });
-    }
+  });
 
+  function saveNewCalculation(newestMath) {
+    $.ajax({
+      type: 'POST',
+      url: '/calculator/new',
+      data: newestMath,
+      success: function(response){
+        console.log(response);
+        $('#answerDisplay').append(response);
+      },
+      error: function(response) {
+        $('#answerDisplay').append('something broke');
+      }
     });
-// console.log(newMathObject);  this object is empty.  I'll need to send my object from within the above submit function
+  }
+});
