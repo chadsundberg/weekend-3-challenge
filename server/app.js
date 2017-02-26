a058dbf426c8232a295c4bbeb4a0c05b4c011e1b
Chad Sundberg <chadsundberg@gmail.com>
2017-02-25 18:31:22 -0600

    stuck on operations

--- server/app.js
+++ server/app.js
@@ -1,1 +1,17 @@
 console.log('sourced');
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+// var calculator = require('.')
+
+app.use(express.static('server/public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+
+app.listen(5000);
---
+++ server/public/client.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+var newMathObject = {};
+var mathOperation = '';
+
+$('button').on('click', function() {
+  var mathOperation = $('.operation').val();
+  return mathOperation;
+});
+
+
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    // var newMathObject = {};
+    // var mathOperation = '';
+
+    newMathObject.firstValue = $('#firstValue').val();
+    newMathObject.secondValue = $('#secondValue').val();
+    newMathObject.operation = mathOperation;
+
+    console.log(newMathObject);
+  });
+// console.log(newMathObject);
+
+});
